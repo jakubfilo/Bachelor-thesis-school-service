5ba39385c472e64d80830396be3ac4a0d5e2b93d
Jakub Filo <jakub.filo@jamf.com>
2024-11-23 10:32:36 +0100

    Inital commit

---
+++ src/main/java/com/jakubfilo/demoserviceb/DemoServiceBApplication.java
@@ -0,0 +1,13 @@
+package com.jakubfilo.demoserviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakubfilo/demoserviceb/DemoServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubfilo.demoserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
